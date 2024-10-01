5327cb91aecd58c086eb803aac232aa654c00186
kevin <kevin@basistheory.com>
2024-10-01 16:46:02 -0600

    ci: run tests and dependency check

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -2,27 +2,51 @@
 
 set -eo pipefail
 
-cat <<EOT > ./ThreeDSTester/Env.plist
+{
+    cat <<EOT > ./ThreeDSTester/Env.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>btApiKey</key>
-	<string>${DEV_BT_API_KEY}</string>
+    <key>btApiKey</key>
+    <string>${DEV_BT_API_KEY}</string>
 </dict>
 </plist>
 EOT
+} || { echo "Error: Failed to create Env.plist"; exit 1; }
 
-xcrun xcodebuild -scheme 'ThreeDSTester' \
--project 'ThreeDSTester/ThreeDSTester.xcodeproj' \
--configuration Debug \
--sdk 'iphonesimulator' \
--destination platform="iOS Simulator,OS=18.0,name=iPhone 14 Pro" \
--derivedDataPath build
-| xcpretty
 
-booted_device_id=$(xcrun simctl list devices booted | grep -oE "[A-F0-9-]{36}")
+echo "Building ThreeDS Library"
 
-xcrun simctl install $booted_device_id build/Build/Products/Debug-iphonesimulator/ThreeDSTester.app
+if ! xcrun xcodebuild -scheme 'ThreeDS' \
+    -project 'ThreeDS/ThreeDS.xcodeproj' \
+    -configuration Debug \
+    -sdk 'iphonesimulator' \
+    -destination platform="iOS Simulator,OS=18.0,name=iPhone 16 Pro" \
+    -derivedDataPath ThreeDS/.build; then
+    echo "Error: xcodebuild failed"
+    exit 1
+fi
 
-maestro --device $booted_device_id test .maestro/tests
+echo "Building ThreeDSTester"
+
+if ! xcrun xcodebuild -scheme 'ThreeDSTester' \
+    -project 'ThreeDSTester/ThreeDSTester.xcodeproj' \
+    -configuration Debug \
+    -sdk 'iphonesimulator' \
+    -destination platform="iOS Simulator,OS=18.0,name=iPhone 16 Pro" \
+    -derivedDataPath .build; then
+    echo "Error: xcodebuild failed"
+    exit 1
+fi
+
+echo "Running maestro tests"
+
+if ! maestro test .maestro/tests; then
+    echo "Error: Maestro tests failed"
+    exit 1
+fi
+
+echo "All operations completed successfully."
+
+exit 0
