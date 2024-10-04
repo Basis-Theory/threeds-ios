e33b87dee1096ff33047c271bc288e87f4ac5230
kevin <kevin@basistheory.com>
2024-10-04 17:50:57 -0600

    feat: adds create session

--- ThreeDS/Sources/ThreeDS/ThreeDSService.swift
+++ ThreeDS/Sources/ThreeDS/ThreeDSService.swift
@@ -1,20 +1,20 @@
 import Foundation
+import OSLog
 import Ravelin3DS
 
-import OSLog
-
-
 @available(iOS 15.0, *)
-public class ThreeDS {
+public class ThreeDSService {
     private var apiKey: String
     private let region: String
     private let locale: String
     private let sandbox: Bool
     private let authenticationEndpoint: String
-    
+
     private var apiBaseUrl: String = "api.basistheory.com"
     private var service: ThreeDS2Service!
-    
+    private var transaction: Transaction!
+    private let decoder = JSONDecoder()
+
     public init(
         apiKey: String, region: String, locale: String, sandbox: Bool, apiBaseUrl: String,
         authenticationEndpoint: String
@@ -25,69 +25,14 @@ public class ThreeDS {
         self.sandbox = sandbox
         self.apiBaseUrl = apiBaseUrl
         self.authenticationEndpoint = authenticationEndpoint
+
+        service = ThreeDS2SDK()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
     }
-    
+
     public static func builder() -> ThreeDSServiceBuilder {
         return ThreeDSServiceBuilder()
     }
-    
-    @discardableResult
-    public func initialize(completion: @escaping ([ThreeDSWarning]?) -> Void) async throws -> String {
-        let endpoint = "https://cdn.basistheory.com/keys/3ds.json"
-        
-        guard let url = URL(string: endpoint) else {
-            OSLogger.log("Invalid URL: \(endpoint)")
-            throw ThreeDsServiceError.invalidURL
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw ThreeDsServiceError.invalidResponse
-        }
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        let keys = try decoder.decode(RavelinKeys.self, from: data)
-        
-        var msg = "initializing"
-        service = ThreeDS2SDK()
-        
-        let configParameters = ConfigParameters()
-        try configParameters.addParam(
-            paramType: .publishableApiKey,
-            paramValue: sandbox ? keys.test : keys.live)
-        
-        
-        do {
-            try service.initialize(configParameters: configParameters, locale: locale, uiCustomization: nil) { success in
-                guard success else {
-                    OSLogger.log("Initialization failed")
-                    completion(nil)
-                    return
-                }
-                
-                OSLogger.log("Initialize SDK complete")
-                
-                do {
-                    let warnings: [ThreeDSWarning] = try self.service.getWarnings().map {
-                        ThreeDSWarning(message: $0.getMessage())
-                    }
-                    
-                    OSLogger.log("\(warnings.map {$0.message}.joined(separator: "\n"))")
-                    completion(warnings)
-                } catch {
-                    OSLogger.log("Error getting warnings: \(error)")
-                    completion(nil)
-                }
-            }
-        } catch {
-            
-        }
-        return msg
-    }
-    
 }
 
 @available(iOS 15.0, *)
@@ -143,7 +88,7 @@ public class ThreeDSServiceBuilder {
         return self
     }
 
-    public func build() throws -> ThreeDS {
+    public func build() throws -> ThreeDSService {
         guard let apiKey = apiKey else {
             throw ThreeDsServiceError.missingApiKey
         }
@@ -157,7 +102,7 @@ public class ThreeDSServiceBuilder {
             : (NSLocale.autoupdatingCurrent.languageCode ?? "en") + "-"
                 + (NSLocale.autoupdatingCurrent.regionCode ?? "US")
 
-        return ThreeDS(
+        return ThreeDSService(
             apiKey: apiKey,
             region: region,
             locale: localeOrDefault,
@@ -173,6 +118,7 @@ enum ThreeDsServiceError: Error {
     case missingAuthenticationEndpoint
     case invalidResponse
     case invalidURL
+    case initializationError
 }
 
 enum RegionEnum {
@@ -195,4 +141,217 @@ public struct ThreeDSWarning {
 struct RavelinKeys: Codable {
     let test: String
     let live: String
+}
+
+public struct CreateThreeDsSessionResponse: Decodable, Encodable {
+    public let id: String
+    public let methodUrl: String
+    public let cardBrand: String
+    public let methodNotificationUrl: String
+    public let directoryServerId: String
+    public let recommendedVersion: String
+}
+
+struct UpdateThreeDsSessionRequest: Codable {
+    let deviceInfo: ThreeDSDeviceInfo
+}
+
+struct ThreeDSDeviceInfo: Codable {
+    var sdkTransactionId: String?
+    var sdkApplicationId: String?
+    var sdkEncryptionData: String?
+    var sdkEphemeralPublicKey: String?
+    var sdkMaxTimeout: String?
+    var sdkReferenceNumber: String?
+    var sdkRenderOptions: ThreeDSMobileSdkRenderOptions?
+}
+
+struct ThreeDSMobileSdkRenderOptions: Codable {
+    var sdkInterface: String?
+    var sdkUiType: [String]?
+}
+
+enum RenderOptions: String {
+    case native = "01"
+
+    func toRavelinCode() -> String {
+        return self.rawValue
+    }
+}
+
+enum UiTypes: String {
+    case textField = "01"
+    case singleSelectField = "02"
+    case multiSelectField = "03"
+    case oob = "04"
+
+    func toRavelinCode() -> String {
+        return self.rawValue
+    }
+}
+
+@available(iOS 15.0, *)
+// Initialization
+extension ThreeDSService {
+    public func initialize(completion: @escaping ([ThreeDSWarning]?) -> Void) async throws {
+        let endpoint = "https://cdn.basistheory.com/keys/3ds.json"
+
+        guard let url = URL(string: endpoint) else {
+            OSLogger.log("Invalid URL: \(endpoint)")
+            throw ThreeDsServiceError.invalidURL
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw ThreeDsServiceError.invalidResponse
+        }
+
+        let keys = try decoder.decode(RavelinKeys.self, from: data)
+
+        let configParameters = ConfigParameters()
+        try configParameters.addParam(
+            paramType: .publishableApiKey,
+            paramValue: sandbox ? keys.test : keys.live)
+
+        OSLogger.log(
+            "\(try configParameters.getParamValue(paramType: .registeredApplicationProviderIdentifiers))"
+        )
+
+        do {
+            // TODO: rewrite to use async/await(?)
+            try service.initialize(
+                configParameters: configParameters, locale: locale, uiCustomization: nil
+            ) { success in
+                guard success else {
+                    completion(nil)
+                    return
+                }
+
+                do {
+                    let warnings: [ThreeDSWarning] = try self.service.getWarnings().map {
+                        ThreeDSWarning(message: $0.getMessage())
+                    }
+
+                    completion(warnings)
+                } catch {
+                    completion(nil)
+                }
+            }
+        } catch {
+            throw ThreeDsServiceError.initializationError
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+extension ThreeDSService {
+    private func makeRequest<T: Codable>(
+        url: URL,
+        method: String,
+        body: Data?,
+        expectedStatusCodes: [Int] = [200, 201]
+    ) async throws -> T {
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(apiKey, forHTTPHeaderField: "BT-API-KEY")
+        request.httpBody = body
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+            expectedStatusCodes.contains(httpResponse.statusCode)
+        else {
+            throw ThreeDsServiceError.invalidResponse
+        }
+
+        let decodedResponse = try decoder.decode(T.self, from: data)
+        return decodedResponse
+    }
+}
+
+@available(iOS 15.0, *)
+// Session Creation
+extension ThreeDSService {
+    public func createSession(
+        tokenId: String
+    ) async throws -> CreateThreeDsSessionResponse {
+        let session = try await _createSession(tokenId: tokenId)
+
+        self.transaction = try service.createTransaction(
+            directoryServerID: session.directoryServerId,
+            messageVersion: session.recommendedVersion)
+
+        let authRequestParams = try self.transaction.getAuthenticationRequestParameters()
+
+        let updatedSession = try await _updateSession(
+            authRequestParams: authRequestParams, sessionId: session.id)
+
+        return updatedSession
+    }
+
+    func _createSession(
+        tokenId: String
+    ) async throws -> CreateThreeDsSessionResponse {
+        let jsonBody: [String: String] = [
+            "pan": tokenId,
+            "device": "app",
+        ]
+
+        let requestBody = try JSONSerialization.data(withJSONObject: jsonBody, options: [])
+
+        guard let createSessionEndpoint = URL(string: "https://\(apiBaseUrl)/3ds/sessions") else {
+            throw ThreeDsServiceError.invalidURL
+        }
+
+        return try await makeRequest(
+            url: createSessionEndpoint,
+            method: "POST",
+            body: requestBody,
+            expectedStatusCodes: [200, 201]
+        )
+    }
+
+    func _updateSession(
+        authRequestParams: AuthenticationRequestParameters, sessionId: String
+    )
+        async throws -> CreateThreeDsSessionResponse
+    {
+        let updateSessionRequestPayload = UpdateThreeDsSessionRequest(
+            deviceInfo: ThreeDSDeviceInfo(
+                sdkTransactionId: authRequestParams.getSDKTransactionID(),
+                sdkApplicationId: authRequestParams.getSDKAppID(),
+                sdkEncryptionData: authRequestParams.getDeviceData(),
+                sdkEphemeralPublicKey: authRequestParams.getSDKEphemeralPublicKey(),
+                sdkMaxTimeout: "05",  // 5 minutes seems to be default
+                sdkReferenceNumber: authRequestParams.getSDKReferenceNumber(),
+                sdkRenderOptions: ThreeDSMobileSdkRenderOptions(
+                    sdkInterface: RenderOptions.native.toRavelinCode(),
+                    sdkUiType: [
+                        UiTypes.textField.toRavelinCode(),
+                        UiTypes.singleSelectField.toRavelinCode(),
+                        UiTypes.multiSelectField.toRavelinCode(),
+                        UiTypes.oob.toRavelinCode(),
+                    ]
+                )
+            )
+        )
+
+        let payload = try JSONEncoder().encode(updateSessionRequestPayload)
+
+        guard
+            let updateSessionEndpoint = URL(
+                string: "https://\(apiBaseUrl)/3ds/sessions/\(sessionId)")
+        else {
+            throw ThreeDsServiceError.invalidURL
+        }
+
+        return try await makeRequest(
+            url: updateSessionEndpoint,
+            method: "PUT",
+            body: payload,
+            expectedStatusCodes: [200]
+        )
+    }
 }
--- ThreeDSTester/ThreeDSTester/ViewController.swift
+++ ThreeDSTester/ThreeDSTester/ViewController.swift
@@ -5,45 +5,91 @@
 //  Created by kevin on 18/9/24.
 //
 
-import UIKit
+import OSLog
 import ThreeDS
+import UIKit
 
 class ViewController: UIViewController {
-    private var threeDS: ThreeDS!
-    
+    private var threeDSService: ThreeDSService!
+
+    @objc func createThreeDsSession() {
+        Task {
+            do {
+                let session = try await threeDSService.createSession(
+                    tokenId: "72cb3d3a-a1c0-4d47-99ce-447c028ff212")
+
+                OSLogger.log("Session created: \(session)")
+
+                let alert = UIAlertController(
+                    title: "Session created",
+                    message: "Session ID: \(session.id)",
+                    preferredStyle: .alert)
+
+                alert.addAction(
+                    UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+
+            } catch {
+                print("\(error) -> \(error.localizedDescription)")
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+
         do {
-            threeDS = try ThreeDS.builder()
+            threeDSService = try ThreeDSService.builder()
                 .withSandbox()
-                .withApiKey("")
+                .withApiKey("key_dev_prod_us_pub_JU4qttr2YJxLJqg64S4Tf5")
+                .withBaseUrl("api.flock-dev.com")
                 .withAuthenticationEndpoint("")
                 .build()
-            
-     
+
             Task {
-                try await threeDS.initialize() { [weak self] warnings in
+                try await threeDSService.initialize { [weak self] warnings in
                     DispatchQueue.main.async {
                         if let warnings = warnings, !warnings.isEmpty {
                             let messages = warnings.map { $0.message }.joined(separator: "\n")
-                            self?.showToast(message: messages)
+
+                            let alert = UIAlertController(
+                                title: "Warning!",
+                                message: messages,
+                                preferredStyle: .alert)
+                            alert.addAction(
+                                UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self?.present(alert, animated: true, completion: nil)
                         } else {
                             self?.showToast(message: "No warnings.")
                         }
                     }
                 }
             }
-            
+
+            let button = UIButton(type: .system)
+            button.setTitle("Create 3DS Session", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .systemBlue
+            button.layer.cornerRadius = 10
+
+            self.view.addSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+                button.widthAnchor.constraint(equalToConstant: 200),
+                button.heightAnchor.constraint(equalToConstant: 44),
+            ])
+
+            button.addTarget(self, action: #selector(createThreeDsSession), for: .touchUpInside)
 
         } catch {
-            
+            // error handling
         }
+
     }
-    
-    
-    
-    
+
 }
 
 extension UIViewController {
@@ -58,22 +104,32 @@ extension UIViewController {
         toastLabel.alpha = 1.0
         toastLabel.layer.cornerRadius = 10
         toastLabel.clipsToBounds = true
-        toastLabel.numberOfLines = 0 // Allow for multi-line
+        toastLabel.numberOfLines = 0  // Allow for multi-line
         toastLabel.lineBreakMode = .byWordWrapping
-        
-         let maxWidth = self.view.frame.size.width - 40
+
+        let maxWidth = self.view.frame.size.width - 40
         let constrainedSize = CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude)
         let expectedSize = toastLabel.sizeThatFits(constrainedSize)
-        
-        toastLabel.frame = CGRect(x: self.view.frame.size.width / 2 - expectedSize.width / 2,
-                                  y: self.view.frame.size.height - 100,
-                                  width: expectedSize.width + 20,
-                                  height: expectedSize.height + 20)
-        
+
+        toastLabel.frame = CGRect(
+            x: self.view.frame.size.width / 2 - expectedSize.width / 2,
+            y: self.view.frame.size.height - 100,
+            width: expectedSize.width + 20,
+            height: expectedSize.height + 20)
+
         self.view.addSubview(toastLabel)
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             toastLabel.removeFromSuperview()
         }
+    }
+}
+
+enum OSLogger {
+    private static let subsystem = Bundle.main.bundleIdentifier!
+    private static let sdk = OSLog(subsystem: subsystem, category: "sdk")
+
+    static func log(_ text: String, type: OSLogType = .default) {
+        os_log("%@", log: OSLogger.sdk, type: type, text)
     }
 }
