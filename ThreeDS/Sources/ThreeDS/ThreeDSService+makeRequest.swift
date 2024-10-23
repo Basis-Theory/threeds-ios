eb9afd0f8bf29bdfee7318cda4ff88cfd505d743
kevin <kevin@basistheory.com>
2024-10-22 19:18:55 -0600

    feat: adds support for custom headers during authentication

--- ThreeDS/Sources/ThreeDS/ThreeDSService+makeRequest.swift
+++ ThreeDS/Sources/ThreeDS/ThreeDSService+makeRequest.swift
@@ -8,13 +8,25 @@ import Foundation
 
 @available(iOS 15.0, *)
 extension ThreeDSService {
+    private func addCustomHeaders(to request: inout URLRequest, headers: [String: String]?) {
+        guard let headers else { return }
+        
+        for (key, value) in headers {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+    }
+    
     func makeRequest<T: Codable>(
         url: URL,
         method: String,
         body: Data?,
-        expectedStatusCodes: [Int] = [200, 201]
+        expectedStatusCodes: [Int] = [200, 201],
+        customHeaders: [String: String] = [:]
     ) async throws -> T {
         var request = URLRequest(url: url)
+        
+        addCustomHeaders(to: &request, headers: customHeaders)
+        
         request.httpMethod = method
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue(self.apiKey, forHTTPHeaderField: "BT-API-KEY")
--- ThreeDS/Sources/ThreeDS/ThreeDSService.swift
+++ ThreeDS/Sources/ThreeDS/ThreeDSService.swift
@@ -20,11 +20,12 @@ public class ThreeDSService {
     var service: ThreeDS2Service!
     var transaction: Transaction!
     var challengeReceiver: ChallengeHandler!
+    var authenticationEndpointHeaders: [String: String]
     var apiKey: String
 
     public init(
         apiKey: String, region: String, locale: String, sandbox: Bool, apiBaseUrl: String,
-        authenticationEndpoint: String
+        authenticationEndpoint: String, authenticationEndpointHeaders: [String: String]
     ) {
         self.apiKey = apiKey
         self.region = region
@@ -32,7 +33,7 @@ public class ThreeDSService {
         self.sandbox = sandbox
         self.apiBaseUrl = apiBaseUrl
         self.authenticationEndpoint = authenticationEndpoint
-
+        self.authenticationEndpointHeaders = authenticationEndpointHeaders
         self.service = ThreeDS2SDK()
         self.decoder.keyDecodingStrategy = .convertFromSnakeCase
     }
