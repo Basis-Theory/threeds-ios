8709d37e500e3024ca963a06876fbdda8a6139c1
Kevin Peraza <kevin@basistheory.com>
2024-10-25 11:12:56 -0600

    fix: re-add ravelin3ds (#14)

--- ThreeDS/Package.swift
+++ ThreeDS/Package.swift
@@ -14,7 +14,11 @@ let package = Package(
             name: "ThreeDS",
             targets: ["ThreeDS"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+            url: "https://github.com/unravelin/ravelin-3ds-sdk-ios-xcframework-distribution",
+            from: "1.1.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
